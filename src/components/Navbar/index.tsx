055f67d9ee1bdd75fd3064e5a93aeedc2db87491
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-01-19 23:06:02 -0300

    feat: Navbar components

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from '@mui/material';
+import * as React from 'react';
+
+export const Navbar: React.FC = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+          texto
+        </Toolbar>
+    </AppBar>
+  )
+}
